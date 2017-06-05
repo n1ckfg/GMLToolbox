bb4edff2b6baba48c208c2b6f86bd176aa60e2d8
Nick Fox-Gieg <nick@fox-gieg.com>
2017-06-05 16:41:08 -0400

    basic gml unity read works

--- unity/Assets/Scripts/GMLContainer.cs
+++ unity/Assets/Scripts/GMLContainer.cs
@@ -7,30 +7,29 @@ using System.IO;
 
 // http://wiki.unity3d.com/index.php?title=Saving_and_Loading_Data:_XmlSerializer
 
-[XmlRoot("MonsterCollection")]
 public class GMLContainer {
 
-    [XmlArray("Monsters"), XmlArrayItem("Monster")]
-    public Monster[] Monsters;
+    //[XmlArray("Monsters"), XmlArrayItem("Monster")]
+    //public Monster[] Monsters;
 
     public void Save(string path) {
-        var serializer = new XmlSerializer(typeof(MonsterContainer));
+        var serializer = new XmlSerializer(typeof(GMLContainer));
         using (var stream = new FileStream(path, FileMode.Create)) {
             serializer.Serialize(stream, this);
         }
     }
 
-    public static MonsterContainer Load(string path) {
-        var serializer = new XmlSerializer(typeof(MonsterContainer));
+    public static GMLContainer Load(string path) {
+        var serializer = new XmlSerializer(typeof(GMLContainer));
         using (var stream = new FileStream(path, FileMode.Open)) {
-            return serializer.Deserialize(stream) as MonsterContainer;
+            return serializer.Deserialize(stream) as GMLContainer;
         }
     }
 
     //Loads the xml directly from the given string. Useful in combination with www.text.
-    public static MonsterContainer LoadFromText(string text) {
-        var serializer = new XmlSerializer(typeof(MonsterContainer));
-        return serializer.Deserialize(new StringReader(text)) as MonsterContainer;
+    public static GMLContainer LoadFromText(string text) {
+        var serializer = new XmlSerializer(typeof(GMLContainer));
+        return serializer.Deserialize(new StringReader(text)) as GMLContainer;
     }
 
 }
--- unity/Assets/Scripts/GMLDraw.cs
+++ unity/Assets/Scripts/GMLDraw.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Xml;
 
 public class GMLDraw : MonoBehaviour {
 
@@ -9,13 +9,86 @@ public class GMLDraw : MonoBehaviour {
     public LADrawing latkd;
     public enum GMLMode { READ_STILL, READ_DRAW, WRITE };
     public GMLMode gmlMode = GMLMode.READ_STILL;
+    public string fileName;
+    public Vector3 globalScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+    [HideInInspector] public GML gml;
+
+    private XmlDocument xml; 
+    private string url;
 
 	void Start() {
+        url = Application.dataPath + "/StreamingAssets/" + fileName;
+        StartCoroutine(LoadGML(url));
+    }
+
+    void Update() {
 		
 	}
-	
-	void Update() {
-		
-	}
+
+    // https://forum.unity3d.com/threads/xml-reading-a-xml-file-in-unity-how-to-do-it.44441/
+
+    IEnumerator LoadGML(string url) {
+        GML gml = new GML();
+        xml = new XmlDocument();
+        xml.Load(url);
+        yield return xml;
+        XmlNode root = xml.FirstChild;
+
+        XmlNode tag = root["tag"];
+        XmlNode header = tag["header"];
+        XmlNode environment = header["environment"];
+        XmlNode up = environment["up"];
+        XmlNode screenBounds = environment["screenBounds"];
+        gml.screenBounds.x = float.Parse(screenBounds["x"].InnerText) * globalScale.x;
+        gml.screenBounds.y = float.Parse(screenBounds["y"].InnerText) * globalScale.y;
+        gml.screenBounds.z = float.Parse(screenBounds["z"].InnerText) * globalScale.z;
+        Debug.Log("dim: " + gml.screenBounds);
+
+        XmlNode drawing = tag["drawing"];
+        for (int i=0; i<drawing.ChildNodes.Count; i++) {
+            GMLStroke stroke = new GMLStroke();
+            XmlNode strokeEl = drawing.ChildNodes[i];
+            for (int j=0; j<strokeEl.ChildNodes.Count; j++) {
+                XmlNode pt = strokeEl.ChildNodes[j];
+                float x = float.Parse(pt["x"].InnerText) * gml.screenBounds.x;
+                float y = float.Parse(pt["y"].InnerText) * gml.screenBounds.y;
+                float z = float.Parse(pt["z"].InnerText) * gml.screenBounds.z;
+                float time = float.Parse(pt["time"].InnerText);
+                GMLPoint point = new GMLPoint();
+                point.pt = new Vector3(x, y, z);
+                point.time = time;
+                stroke.points.Add(point);
+            }
+            gml.strokes.Add(stroke);
+        }
+
+        for (int i=0; i<gml.strokes.Count; i++) {
+            latkd.makeCurve(gml.strokes[i].getPoints());
+        }
+    }
+
+    public class GML {
+        public Vector3 screenBounds = Vector3.one;
+        public Vector3 up = new Vector3(0f, 1f, 0f);
+        public List<GMLStroke> strokes = new List<GMLStroke>();
+    }
+
+    public class GMLStroke {
+        public List<GMLPoint> points = new List<GMLPoint>();
+
+        public List<Vector3> getPoints() {
+            List<Vector3> returns = new List<Vector3>();
+            for (int i=0; i<points.Count; i++) {
+               returns.Add(points[i].pt);
+            }
+            return returns;
+        }
+    }
+
+    public class GMLPoint {
+        public Vector3 pt = Vector3.zero;
+        public float time = 0f;
+    }
 
 }
