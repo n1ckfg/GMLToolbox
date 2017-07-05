34e5edada067b879176dabb2474c221c97f727bd
Nick Fox-Gieg <nick@fox-gieg.com>
2017-07-04 21:25:47 -0400

    cleanup

--- unity/Assets/Scripts/GMLDraw.cs
+++ unity/Assets/Scripts/GMLDraw.cs
@@ -104,3 +104,26 @@ public class GMLDraw : MonoBehaviour {
     }
 
 }
+
+public class GML {
+	public Vector3 screenBounds = Vector3.one;
+	public Vector3 up = new Vector3(0f, 1f, 0f);
+	public List<GMLStroke> strokes = new List<GMLStroke>();
+}
+
+public class GMLStroke {
+	public List<GMLPoint> points = new List<GMLPoint>();
+
+	public List<Vector3> getPoints() {
+		List<Vector3> returns = new List<Vector3>();
+		for (int i = 0; i < points.Count; i++) {
+			returns.Add(points[i].pt);
+		}
+		return returns;
+	}
+}
+
+public class GMLPoint {
+	public Vector3 pt = Vector3.zero;
+	public float time = 0f;
+}
